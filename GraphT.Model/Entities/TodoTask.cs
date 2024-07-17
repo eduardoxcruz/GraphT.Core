964eacd13fc4b9fc54a77bf6e33ac229bfb8df9a
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-17 01:51:10 -0600

    Add TodoTask.cs

---
+++ GraphT.Model/Entities/TodoTask.cs
@@ -0,0 +1,13 @@
+﻿namespace GraphT.Model.Entities;
+
+public class TodoTask
+{
+	public string Id { get; private set; }
+	public string Name { get; set; }
+
+	protected TodoTask(string name)
+	{
+		Id = Guid.NewGuid().ToString();
+		Name = name;
+	}
+}
