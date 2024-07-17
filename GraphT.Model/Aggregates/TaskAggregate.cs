8d26782629cdf498cc68e215263ec09d47f4b806
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-17 01:54:31 -0600

    Add TaskAggregate.cs

---
+++ GraphT.Model/Aggregates/TaskAggregate.cs
@@ -0,0 +1,72 @@
+﻿using GraphT.Model.Entities;
+using GraphT.Model.ValueObjects;
+
+namespace GraphT.Model.Aggregates;
+
+public class TaskAggregate : TodoTask
+{
+	private bool _isFun;
+	private bool _isProductive;
+	private readonly HashSet<TodoTask> _upstreams = null!;
+	private readonly HashSet<TodoTask> _downstreams = null!;
+	public bool IsFun
+	{
+		get => _isFun;
+		set
+		{
+			_isFun = value;
+			UpdateRelevance();
+		}
+	}
+	public bool IsProductive
+	{
+		get => _isProductive;
+		set
+		{
+			_isProductive = value;
+			UpdateRelevance();
+		}
+	}
+	public float Progress => GetProgress();
+	public Complexity Complexity { get; set; }
+	public Priority Priority { get; set; }
+	public Status Status { get; set; }
+	public Relevance Relevance { get; private set; }
+	public IReadOnlySet<TodoTask> Upstreams => _upstreams;
+	public IReadOnlySet<TodoTask> Downstreams => _downstreams;
+
+	private TaskAggregate() : base(String.Empty) { }
+
+	protected TaskAggregate(string name, 
+							bool isFun, 
+							bool isProductive, 
+							Complexity complexity = Complexity.Indefinite, 
+							Priority priority = Priority.MentalClutter, 
+							Status status = Status.Backlog) : base(name)
+	{
+		_isFun = isFun;
+		_isProductive = isProductive;
+		_upstreams = new HashSet<TodoTask>();
+		_downstreams = new HashSet<TodoTask>();
+		Complexity = complexity;
+		Priority = priority;
+		Status = status;
+		UpdateRelevance();
+	}
+
+	private float GetProgress()
+	{
+		throw new NotImplementedException();
+	}
+
+	private void UpdateRelevance()
+	{
+		this.Relevance = IsFun switch
+		{
+			true when IsProductive => Relevance.Purposeful,
+			false when IsProductive => Relevance.Necessary,
+			true when !IsProductive => Relevance.Entertaining,
+			_ => Relevance.Superfluous
+		};
+	}
+}
