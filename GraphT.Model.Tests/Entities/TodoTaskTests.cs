41d221153f501dcaf07565f8af7b08a4f9d3de5f
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-17 20:26:27 -0600

    Add TodoTaskTests.cs

---
+++ GraphT.Model.Tests/Entities/TodoTaskTests.cs
@@ -0,0 +1,40 @@
+﻿using GraphT.Model.Entities;
+
+namespace GraphT.Model.Tests.Entities;
+
+public class TodoTaskTests
+{
+	private class TestableTodoTask(string name) : TodoTask(name);
+
+	[Fact]
+	public void Constructor_ShouldAssignNewGuidToId()
+	{
+		string taskName = "Test Task";
+		
+		TestableTodoTask task = new(taskName);
+
+		Assert.NotEqual(Guid.Empty, task.Id);
+	}
+
+	[Fact]
+	public void Constructor_ShouldAssignName()
+	{
+		string taskName = "Test Task";
+		
+		TestableTodoTask task = new(taskName);
+		
+		Assert.Equal(taskName, task.Name);
+	}
+
+	[Fact]
+	public void Name_ShouldBeAssignable()
+	{
+		string taskName = "Initial Name";
+		TestableTodoTask task = new(taskName);
+		string newName = "Updated Name";
+
+		task.Name = newName;
+
+		Assert.Equal(newName, task.Name);
+	}
+}
