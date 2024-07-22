00719f80a1ca92e2221e9c0ee4afab6e3222bc3b
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-21 22:59:53 -0600

    Add ComplexityExtensions in Complexity.cs

--- GraphT.Model/ValueObjects/Complexity.cs
+++ GraphT.Model/ValueObjects/Complexity.cs
@@ -6,3 +6,18 @@ public enum Complexity
 	Low = 1,
 	High = 2
 }
+
+public static class ComplexityExtensions
+{
+	public static string FormatedName(this Complexity complexity)
+	{
+		string formatedName = complexity switch
+		{
+			Complexity.Indefinite => "\ud83e\udd37 Indefinite",
+			Complexity.Low => "\ud83e\udd71 Low",
+			_ => "\ud83d\ude31 High"
+		};
+		
+		return formatedName;
+	}
+}
