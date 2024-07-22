5062dce06fdccb835e64481cff970f69a05d3bfa
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-21 19:56:37 -0600

    Create ITaskReadableRepository.cs

---
+++ GraphT.Model/Repositories/ITaskReadableRepository.cs
@@ -0,0 +1,11 @@
+﻿using GraphT.Model.Aggregates;
+using GraphT.Model.Entities;
+
+namespace GraphT.Model.Repositories;
+
+public interface ITaskReadableRepository
+{
+	public TaskAggregate GetTask(Guid taskId);
+	public HashSet<TodoTask> GetUpstreams(Guid taskId);
+	public HashSet<TodoTask> GetDownstreams(Guid taskId);
+}
