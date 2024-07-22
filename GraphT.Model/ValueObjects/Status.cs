d1e3f59ff570d5f87119cfc0e9754ad38d05ac39
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-21 23:24:10 -0600

    Add StatusExtensions class to Status.cs

--- GraphT.Model/ValueObjects/Status.cs
+++ GraphT.Model/ValueObjects/Status.cs
@@ -9,3 +9,21 @@ public enum Status
 	Dropped = 4,
 	Completed = 5
 }
+
+public static class StatusExtensions
+{
+	public static string FormatedName(this Status status)
+	{
+		string formatedName = status switch
+		{
+			Status.Backlog => "\ud83d\udcdd Backlog",
+			Status.ReadyToStart => "\ud83d\udc4c Ready To Start",
+			Status.InProgress => "\u25b6 In Progress",
+			Status.Paused => "\u23f8 Paused",
+			Status.Dropped => "\ud83d\uddd1 Dropped",
+			_ => "\u2705 Completed"
+		};
+
+		return formatedName;
+	}
+}
