890126cf8483c28f23f0f656368bcbf28e4ecb32
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-21 23:07:29 -0600

    Add RelevanceExtensions class in Relevance.cs

--- GraphT.Model/ValueObjects/Relevance.cs
+++ GraphT.Model/ValueObjects/Relevance.cs
@@ -6,3 +6,19 @@ public enum Relevance {
 	Necessary = 2,
 	Purposeful = 3 
 }
+
+public static class RelevanceExtensions
+{
+	public static string FormatedName(this Relevance relevance)
+	{
+		string formatedName = relevance switch
+		{
+			Relevance.Superfluous => "\ud83d\ude12 Superfluous",
+			Relevance.Entertaining => "\ud83e\udd24 Entertaining",
+			Relevance.Necessary => "\ud83e\uddd0 Necessary",
+			_ => "\ud83d\ude0e Purposeful"
+		};
+
+		return formatedName;
+	}
+}
