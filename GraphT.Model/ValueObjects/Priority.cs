ecc85317a134e15f1529046d1c09b8335a25a1d5
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-21 20:34:27 -0600

    Add PriorityExtensions class in Priority.cs

--- GraphT.Model/ValueObjects/Priority.cs
+++ GraphT.Model/ValueObjects/Priority.cs
@@ -7,3 +7,19 @@ public enum Priority
 	DoItNow = 2,
 	DropEverythingElse = 3 
 }
+
+public static class PriorityExtensions
+{
+	public static string FormatedName(this Priority priority)
+	{
+		string formatedName = priority switch
+		{
+			Priority.MentalClutter => "\ud83d\udecc Mental Clutter",
+			Priority.ThinkAboutIt => "\ud83e\uddcd\u200d\u2642\ufe0f Think About It",
+			Priority.DoItNow => "\ud83c\udfc3\u200d\u2642\ufe0f Do It Now",
+			_ => "\ud83d\udeb4\u200d\u2642\ufe0f Drop Everything Else"
+		};
+
+		return formatedName;
+	} 
+}
