685e1a8fa9d6c406e1ab7925e6de47d580ffa30d
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-23 23:05:00 -0600

    Remove readonly keyword from _task field in LifeAreaAggregate.cs

--- GraphT.Model/Aggregates/LifeAreaAggregate.cs
+++ GraphT.Model/Aggregates/LifeAreaAggregate.cs
@@ -4,7 +4,7 @@ namespace GraphT.Model.Aggregates;
 
 public class LifeAreaAggregate : LifeArea
 {
-	private readonly HashSet<TodoTask> _tasks;
+	private HashSet<TodoTask> _tasks;
 	public IReadOnlySet<TodoTask> Tasks => _tasks;
 
 	private LifeAreaAggregate(string name) : base(name)
