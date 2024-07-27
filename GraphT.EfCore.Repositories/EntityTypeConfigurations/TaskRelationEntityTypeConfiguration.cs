730f88e706a1e9afcc939de0284f3ea83d7a09e6
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-26 22:53:59 -0600

    Create TaskRelationEntityTypeConfiguration.cs

---
+++ GraphT.EfCore.Repositories/EntityTypeConfigurations/TaskRelationEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GraphT.EfCore.Repositories.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GraphT.EfCore.Repositories.EntityTypeConfigurations;
+
+public class TaskRelationEntityTypeConfiguration : IEntityTypeConfiguration<TaskRelationModel>
+{
+	public void Configure(EntityTypeBuilder<TaskRelationModel> builder)
+	{
+		builder.HasKey(taskRelation => new { taskRelation.UpstreamTaskId, taskRelation.DownstreamTaskId });
+	}
+}
