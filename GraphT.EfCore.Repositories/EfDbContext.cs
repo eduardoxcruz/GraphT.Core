6a2858aa5a1726680b587f68b1447853f83fa19e
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-26 22:30:04 -0600

    Create EfDbContext.cs

---
+++ GraphT.EfCore.Repositories/EfDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphT.EfCore.Repositories;
+
+public partial class EfDbContext : DbContext
+{
+	public EfDbContext(DbContextOptions<EfDbContext> options) : base(options) { }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		OnModelCreatingPartial(modelBuilder);
+	}
+
+	partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
