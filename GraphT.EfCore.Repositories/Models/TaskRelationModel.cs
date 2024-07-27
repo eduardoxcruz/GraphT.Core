c08baf2221733f84a1c25410bdbc5e00399df536
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-26 22:46:18 -0600

    Create TaskRelationModel.cs

---
+++ GraphT.EfCore.Repositories/Models/TaskRelationModel.cs
@@ -0,0 +1,12 @@
+﻿using GraphT.Model.Entities;
+
+namespace GraphT.EfCore.Repositories.Models;
+
+public class TaskRelationModel
+{
+	public Guid UpstreamTaskId { get; set; }
+	public Guid DownstreamTaskId { get; set; }
+
+	public TodoTask UpstreamTask { get; set; }
+	public TodoTask DownstreamTask { get; set; }
+}
