bc0ad66cff38b2423671ef4eb83324772a8a10ba
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 00:07:35 -0600

    Create LastTaskLogSpecification.cs in Model.Services.Specifications

---
+++ GraphT.Model/Services/Specifications/LastTaskLogSpecification.cs
@@ -0,0 +1,13 @@
+﻿using GraphT.Model.ValueObjects;
+
+using SeedWork;
+
+namespace GraphT.Model.Services.Specifications;
+
+public sealed class LastTaskLogSpecification : BaseSpecification<TaskLog>
+{
+	public LastTaskLogSpecification(Guid taskId) : base(taskLog => taskLog.TaskId.Equals(taskId))
+	{
+		ApplyOrderByDescending(taskLog => taskLog.DateTime);
+	}
+}
