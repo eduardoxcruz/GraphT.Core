f9c40c1567bc6719ef8d63d7a8e3bd74614b2cb2
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 17:55:09 -0600

    Return TimeSpan.Zero instead of null in GetTimeSpend(...)
    In TimeSpendCalculatorService.cs

--- GraphT.Model/Services/TimeSpendCalculatorService.cs
+++ GraphT.Model/Services/TimeSpendCalculatorService.cs
@@ -7,20 +7,20 @@ namespace GraphT.Model.Services;
 
 public static class TimeSpendCalculatorService
 {
-	public static async ValueTask<TimeSpan?> GetTimeSpend(
+	public static async ValueTask<TimeSpan> GetTimeSpend(
 		Guid taskId, 
 		Status newStatus, 
 		DateTimeOffset newDateTime, 
 		IUnitOfWork unitOfWork)
 	{
-		if (newStatus is Status.Created or Status.Backlog or Status.ReadyToStart or Status.InProgress) return null;
+		if (newStatus is Status.Created or Status.Backlog or Status.ReadyToStart or Status.InProgress) return TimeSpan.Zero;
 		
 		TaskLog? lastLog = (await unitOfWork.Repository<TaskLog>().FindAsync(new LastTaskLogSpecification(taskId)))
 			.FirstOrDefault();
 
-		if (lastLog is null) return null;
+		if (lastLog is null) return TimeSpan.Zero;
 		
-		if (lastLog.Status is Status.Paused or Status.Dropped or Status.Completed) return null;
+		if (lastLog.Status is Status.Paused or Status.Dropped or Status.Completed) return TimeSpan.Zero;
 		
 		return newDateTime - lastLog.DateTime;
 	}
