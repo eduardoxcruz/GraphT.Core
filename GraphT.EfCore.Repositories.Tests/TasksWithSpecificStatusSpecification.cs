1aaa651e0bce863b0a55280df648807264f1b110
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-30 22:30:16 -0600

    Add TasksWithSpecificStatusSpecification.cs in EfCore.Repositories.Tests

---
+++ GraphT.EfCore.Repositories.Tests/TasksWithSpecificStatusSpecification.cs
@@ -0,0 +1,14 @@
+﻿using GraphT.Model.Entities;
+using GraphT.Model.ValueObjects;
+
+using SeedWork;
+
+namespace GraphT.EfCore.Repositories.Tests;
+
+public sealed class TasksWithSpecificStatusSpecification : BaseSpecification<TodoTask>
+{
+	public TasksWithSpecificStatusSpecification(Status taskStatus) : base(todoTask => todoTask.Status == taskStatus)
+	{
+		ApplyOrderBy(todoTask => todoTask.Name);
+	}
+}
