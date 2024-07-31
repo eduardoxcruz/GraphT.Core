4e1ba87816d665bf93bc87369cf986768f6ffeeb
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 16:48:15 -0600

    Add DependencyContainer.cs in EfCore.Repositories

---
+++ GraphT.EfCore.Repositories/DependencyContainer.cs
@@ -0,0 +1,28 @@
+using GraphT.Model.Aggregates;
+using GraphT.Model.Entities;
+using GraphT.Model.ValueObjects;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using SeedWork;
+
+namespace GraphT.EfCore.Repositories;
+
+public static class DependencyContainer
+{
+	public static IServiceCollection AddGraphTEfCoreRepositories(
+		this IServiceCollection services,
+		IConfiguration configuration,
+		string connectionString)
+	{
+		services.AddDbContext<EfDbContext>(options => options.UseSqlServer(configuration.GetConnectionString(connectionString)));
+		services.AddScoped<IRepository<TodoTask>, Repository<TodoTask>>();
+		services.AddScoped<IRepository<TaskAggregate>, Repository<TaskAggregate>>();
+		services.AddScoped<IRepository<TaskLog>, Repository<TaskLog>>();
+		services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+		return services;
+	}
+}
