57c94c1d17b7e661ffa67dae5f8c9ba97fa89620
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 23:56:19 -0600

    Add GetFinishedTasks and GetUnfinishedTasks presenters to DependencyContainer.cs

--- GraphT.Presenters/DependencyContainer.cs
+++ GraphT.Presenters/DependencyContainer.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using GraphT.Presenters.GetFinishedOrUnfinishedTasks;
+using GraphT.UseCases.GetFinishedAndUnfinishedTasks;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GraphT.Presenters
 {
     public static class DependencyContainer
     {
-        public static IServiceCollection AddGraphTPresenters(
-            this IServiceCollection services)
+        public static IServiceCollection AddGraphTPresenters(this IServiceCollection services)
         {
+	        services.AddScoped<IGetFinishedTasksOutputPort, GetFinishedTasksPresenter>();
+	        services.AddScoped<IGetUnfinishedTasksOutputPort, GetUnfinishedTasksPresenter>();
             return services;
         }
     }
