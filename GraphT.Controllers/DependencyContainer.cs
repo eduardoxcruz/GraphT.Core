50a81e8cf31ba4b54654f67bab237197c57a7f23
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 23:58:11 -0600

    Add GetFinishedTasks and GetUnfinishedTasks controllers to DependencyContainer.cs

--- GraphT.Controllers/DependencyContainer.cs
+++ GraphT.Controllers/DependencyContainer.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using GraphT.Controllers.GetFinishedOrUnfinishedTasks;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GraphT.Controllers
 {
     public static class DependencyContainer
     {
-        public static IServiceCollection AddGraphTControllers(
-            this IServiceCollection services)
+        public static IServiceCollection AddGraphTControllers(this IServiceCollection services)
         {
+	        services.AddScoped<IGetFinishedTasksController, GetFinishedTasksController>();
+	        services.AddScoped<IGetUnfinishedTasksController, GetUnfinishedTasksController>();
             return services;
         }
     }
