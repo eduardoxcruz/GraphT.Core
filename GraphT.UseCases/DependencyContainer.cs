5a12fc3c50cd1a03a354bb2981e879c14ddb7bd5
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 23:55:16 -0600

    Add GetUnfinishedTasks and GetFinishedTasks use cases to DependencyContainer.cs

--- GraphT.UseCases/DependencyContainer.cs
+++ GraphT.UseCases/DependencyContainer.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using GraphT.UseCases.GetFinishedAndUnfinishedTasks;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GraphT.UseCases
 {
     public static class DependencyContainer
     {
-        public static IServiceCollection AddGraphTUseCases(
-            this IServiceCollection services)
+        public static IServiceCollection AddGraphTUseCases(this IServiceCollection services)
         {
+	        services.AddScoped<IGetFinishedTasksInputPort, GetFinishedTasks>();
+	        services.AddScoped<IGetUnfinishedTasksInputPort, GetUnfinishedTasks>();
             return services;
         }
     }
