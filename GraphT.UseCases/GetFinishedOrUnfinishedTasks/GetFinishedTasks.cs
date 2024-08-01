95bd91fe03d1d450af38df8118297ed73e4a6107
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 23:54:25 -0600

    Create GetFinishedTasks.cs in UseCases

---
+++ GraphT.UseCases/GetFinishedOrUnfinishedTasks/GetFinishedTasks.cs
@@ -0,0 +1,30 @@
+﻿using GraphT.Model.Entities;
+using GraphT.Model.Services.Specifications;
+
+using SeedWork;
+
+namespace GraphT.UseCases.GetFinishedAndUnfinishedTasks;
+
+public interface IGetFinishedTasksInputPort : IPort<GetTasksFromNameDto> { }
+
+public interface IGetFinishedTasksOutputPort : IPort<OnlyTodoTaskPagedListDto> { }
+
+internal class GetFinishedTasks : IGetFinishedTasksInputPort
+{
+	private readonly IGetFinishedTasksOutputPort _outputPort;
+	private readonly IUnitOfWork _unitOfWork;
+
+	public GetFinishedTasks(IGetFinishedTasksOutputPort outputPort, IUnitOfWork unitOfWork)
+	{
+		_outputPort = outputPort;
+		_unitOfWork = unitOfWork;
+	}
+
+	public async ValueTask Handle(GetTasksFromNameDto dto)
+	{
+		FinishedTasksSpecification specification = new(dto.TaskName, dto.PagingParams);
+		PagedList<TodoTask> tasks = await _unitOfWork.Repository<TodoTask>().FindAsync(specification);
+		OnlyTodoTaskPagedListDto onlyTodoTaskPagedListDto = new(tasks);
+		await _outputPort.Handle(onlyTodoTaskPagedListDto);
+	}
+}
