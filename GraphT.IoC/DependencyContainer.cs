678198c3b5fb60d5f0f0e5b4d8969511e53c3656
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-08-01 00:23:10 -0600

    Fix indentation in IoC.DependencyContainer.cs

--- GraphT.IoC/DependencyContainer.cs
+++ GraphT.IoC/DependencyContainer.cs
@@ -6,22 +6,21 @@ using GraphT.UseCases;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace GraphT.IoC
+namespace GraphT.IoC;
+
+public static class DependencyContainer
 {
-    public static class DependencyContainer
-    {
-        public static IServiceCollection AddGraphTServices(
-            this IServiceCollection services, 
-            IConfiguration configuration,
-            string connectionStringName
-        )
-        {
-            services
-                .AddGraphTUseCases()
-                .AddGraphTPresenters()
-                .AddGraphTControllers()
-                .AddGraphTEfCoreRepositories(configuration, connectionStringName);
-            return services;
-        }
-    }
+	public static IServiceCollection AddGraphTServices(
+		this IServiceCollection services,
+		IConfiguration configuration,
+		string connectionStringName
+	)
+	{
+		services
+			.AddGraphTUseCases()
+			.AddGraphTPresenters()
+			.AddGraphTControllers()
+			.AddGraphTEfCoreRepositories(configuration, connectionStringName);
+		return services;
+	}
 }
