1fea2d2e34ebdc2ed3358a3b941b3965cab0fbbf
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 23:56:37 -0600

    Create GetFinishedTasksController.cs

---
+++ GraphT.Controllers/GetFinishedOrUnfinishedTasks/GetFinishedTasksController.cs
@@ -0,0 +1,26 @@
+﻿using GraphT.UseCases;
+using GraphT.UseCases.GetFinishedAndUnfinishedTasks;
+
+using SeedWork;
+
+namespace GraphT.Controllers.GetFinishedOrUnfinishedTasks;
+
+public interface IGetFinishedTasksController : IControllerTOutTIn<OnlyTodoTaskPagedListDto, GetTasksFromNameDto> {}
+
+public class GetFinishedTasksController : IGetFinishedTasksController
+{
+	private readonly IGetFinishedTasksInputPort _inputPort;
+	private readonly IGetFinishedTasksOutputPort _outputPort;
+
+	public GetFinishedTasksController(IGetFinishedTasksInputPort inputPort, IGetFinishedTasksOutputPort outputPort)
+	{
+		_inputPort = inputPort;
+		_outputPort = outputPort;
+	}
+    
+	public async ValueTask<OnlyTodoTaskPagedListDto> RunUseCase(GetTasksFromNameDto inputDto)
+	{
+		await _inputPort.Handle(inputDto);
+		return ((IPresenter<OnlyTodoTaskPagedListDto>)_outputPort).Content;
+	}
+}
