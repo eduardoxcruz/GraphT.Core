1afcb814702ff114206106af1df02c162b03f8e6
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 23:55:44 -0600

    Create GetFinishedTasksPresenter.cs in Presenters

---
+++ GraphT.Presenters/GetFinishedOrUnfinishedTasks/GetFinishedTasksPresenter.cs
@@ -0,0 +1,17 @@
+﻿using GraphT.UseCases;
+using GraphT.UseCases.GetFinishedAndUnfinishedTasks;
+
+using SeedWork;
+
+namespace GraphT.Presenters.GetFinishedOrUnfinishedTasks;
+
+public class GetFinishedTasksPresenter : IPresenter<OnlyTodoTaskPagedListDto>, IGetFinishedTasksOutputPort
+{
+	public OnlyTodoTaskPagedListDto Content { get; private set; }
+	
+	public ValueTask Handle(OnlyTodoTaskPagedListDto dto)
+	{
+		Content = dto;
+		return ValueTask.CompletedTask;
+	}
+}
