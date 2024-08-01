dd6f08a24f77fa2e71d27a20d05dce5d3604f398
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-08-01 00:15:49 -0600

    Rename GetFinishedTasks.cs to GetFinishedTasks.UseCase

--- GraphT.UseCases/GetFinishedTasks/UseCase.cs
+++ GraphT.UseCases/GetFinishedTasks/UseCase.cs
@@ -3,18 +3,18 @@ using GraphT.Model.Services.Specifications;
 
 using SeedWork;
 
-namespace GraphT.UseCases.GetFinishedAndUnfinishedTasks;
+namespace GraphT.UseCases.GetFinishedTasks;
 
-public interface IGetFinishedTasksInputPort : IPort<GetTasksFromNameDto> { }
+public interface IInputPort : IPort<GetTasksFromNameDto> { }
 
-public interface IGetFinishedTasksOutputPort : IPort<OnlyTodoTaskPagedListDto> { }
+public interface IOutputPort : IPort<OnlyTodoTaskPagedListDto> { }
 
-internal class GetFinishedTasks : IGetFinishedTasksInputPort
+internal class UseCase : IInputPort
 {
-	private readonly IGetFinishedTasksOutputPort _outputPort;
+	private readonly IOutputPort _outputPort;
 	private readonly IUnitOfWork _unitOfWork;
 
-	public GetFinishedTasks(IGetFinishedTasksOutputPort outputPort, IUnitOfWork unitOfWork)
+	public UseCase(IOutputPort outputPort, IUnitOfWork unitOfWork)
 	{
 		_outputPort = outputPort;
 		_unitOfWork = unitOfWork;
