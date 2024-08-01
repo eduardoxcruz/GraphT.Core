4613caa84f762262bab93090d7e969a2cd33988e
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-07-31 23:44:33 -0600

    Create OnlyTodoTaskPagedListDto.cs in UseCases

---
+++ GraphT.UseCases/OnlyTodoTaskPagedListDto.cs
@@ -0,0 +1,10 @@
+﻿using GraphT.Model.Entities;
+
+using SeedWork;
+
+namespace GraphT.UseCases;
+
+public class OnlyTodoTaskPagedListDto(PagedList<TodoTask> todoTasks)
+{
+	public readonly PagedList<TodoTask> TodoTasks = todoTasks;
+}
