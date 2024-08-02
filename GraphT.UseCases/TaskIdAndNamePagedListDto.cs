790caaf3e418004beb69b31616b205d01c8cee9b
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-08-02 00:31:39 -0600

    Create TaskIdAndNamePagedListDto.cs in UseCases

---
+++ GraphT.UseCases/TaskIdAndNamePagedListDto.cs
@@ -0,0 +1,13 @@
+﻿using SeedWork;
+
+namespace GraphT.UseCases;
+
+public class TaskIdAndNamePagedListDto
+{
+	public PagedList<TaskIdAndName> Tasks { get; set; }
+
+	public TaskIdAndNamePagedListDto(PagedList<TaskIdAndName> tasks)
+	{
+		Tasks = tasks;
+	}
+}
