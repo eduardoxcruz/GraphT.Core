ffc5f8773b61cca644a02a801537a869315143a3
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-04 14:54:59 -0600

    Delete LifeArea.cs entity

--- GraphT.Model/Aggregates/LifeAreaAggregate.cs
+++ GraphT.Model/Aggregates/LifeAreaAggregate.cs
@@ -1,14 +1,14 @@
-﻿using GraphT.Model.Entities;
+﻿namespace GraphT.Model.Aggregates;
 
-namespace GraphT.Model.Aggregates;
-
-public class LifeAreaAggregate : LifeArea
+public class LifeAreaAggregate
 {
+	public string Name { get; set; }
 	private HashSet<TaskAggregate> _tasks;
 	public IReadOnlySet<TaskAggregate> Tasks => _tasks;
 
-	private LifeAreaAggregate(string name) : base(name)
+	private LifeAreaAggregate(string name)
 	{
+		Name = name;
 		_tasks = new HashSet<TaskAggregate>();
 	}
 }
