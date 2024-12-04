a50e55c06670c8939aee87b3588ea7011da7682d
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-04 15:19:53 -0600

    Add Id property to LifeAreaAggregate.cs

--- GraphT.Model/Aggregates/LifeAreaAggregate.cs
+++ GraphT.Model/Aggregates/LifeAreaAggregate.cs
@@ -2,12 +2,14 @@
 
 public class LifeAreaAggregate
 {
+	public Guid Id { get; private set; }
 	public string Name { get; set; }
 	private HashSet<TaskAggregate> _tasks;
 	public IReadOnlySet<TaskAggregate> Tasks => _tasks;
 
 	private LifeAreaAggregate(string name)
 	{
+		Id = Guid.NewGuid();
 		Name = name;
 		_tasks = new HashSet<TaskAggregate>();
 	}
