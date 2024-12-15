b495737a69723f4b94b19e5aa3a779d3a756636b
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2024-12-15 15:35:21 -0600

    Add TaskNotFoundException.cs

---
+++ GraphT.Model/Exceptions/TaskNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿namespace GraphT.Model.Exceptions;
+
+public class TaskNotFoundException : Exception
+{
+	public Guid Id { get; }
+	
+	public TaskNotFoundException(Guid id)
+	{
+		Id = id;
+	}
+	
+	public TaskNotFoundException(string message, Guid id) : base(message)
+	{
+		Id = id;
+	}
+
+	public TaskNotFoundException(string message, Exception inner, Guid id)
+		: base(message, inner)
+	{
+		Id = id;
+	}
+}
