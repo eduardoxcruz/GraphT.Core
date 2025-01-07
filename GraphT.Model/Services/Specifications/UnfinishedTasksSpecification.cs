418b56b565f118562e742ef2207ceee494b7e1e7
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-07 14:30:13 -0600

    Fix criteria in UnfinishedTasksSpecification.cs

--- GraphT.Model/Services/Specifications/UnfinishedTasksSpecification.cs
+++ GraphT.Model/Services/Specifications/UnfinishedTasksSpecification.cs
@@ -8,10 +8,7 @@ namespace GraphT.Model.Services.Specifications;
 public sealed class UnfinishedTasksSpecification : BaseSpecification<TaskAggregate>
 {
 	public UnfinishedTasksSpecification(string? name, PagingParams pagingParams) : 
-		base(task => 
-			(task.Status == Status.Completed) && 
-			(task.Status == Status.Dropped) && 
-			(name == null || task.Name.Contains(name) ))
+		base(task => (task.Status != Status.Completed) && (name == null || task.Name.Contains(name)))
 	{
 		ApplyPaging(pagingParams.PageNumber, pagingParams.PageSize);
 	}
