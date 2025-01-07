9330e3ce4bf11faa2ed579ce60b384f4702e0e63
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-07 12:39:33 -0600

    Fix using in Controllers.DependencyContainer

--- GraphT.Controllers/DependencyContainer.cs
+++ GraphT.Controllers/DependencyContainer.cs
@@ -4,15 +4,13 @@ using GraphT.Controllers.UpdateTask;
 
 using Microsoft.Extensions.DependencyInjection;
 
-using Controller = GraphT.Controllers.GetFinishedTasks.Controller;
-
 namespace GraphT.Controllers;
 
 public static class DependencyContainer
 {
 	public static IServiceCollection AddGraphTControllers(this IServiceCollection services)
 	{
-		services.AddScoped<IGetFinishedTasksController, Controller>();
+		services.AddScoped<IGetFinishedTasksController, GetFinishedTasks.Controller>();
 		services.AddScoped<IGetUnfinishedTasksController, GetUnfinishedTasks.Controller>();
 		services.AddScoped<IUpdateTaskController, UpdateTask.Controller>();
 		
