f28e4113a8bf215c21583efa82b55af3014bd035
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-07 20:00:40 -0600

    Delete GetFinishedTasks controller

--- GraphT.Controllers/DependencyContainer.cs
+++ GraphT.Controllers/DependencyContainer.cs
@@ -1,10 +1,7 @@
 ﻿using GraphT.Controllers.AddNewTask;
-using GraphT.Controllers.GetFinishedTasks;
 using GraphT.Controllers.UpdateTask;
 
 using Microsoft.Extensions.DependencyInjection;
-
-using Controller = GraphT.Controllers.GetFinishedTasks.Controller;
 
 namespace GraphT.Controllers;
 
@@ -12,7 +9,6 @@ public static class DependencyContainer
 {
 	public static IServiceCollection AddGraphTControllers(this IServiceCollection services)
 	{
-		services.AddScoped<IGetFinishedTasksController, Controller>();
 		services.AddScoped<IUpdateTaskController, UpdateTask.Controller>();
 		services.AddScoped<IAddNewTaskController, AddNewTask.Controller>();
 		
