b69bf8227d8268e2d7542e43be25266a70272363
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-09 23:07:04 -0600

    Rename and refactor UnfinishedTasksSpecification to FindReadyToStartTasksSpecification
    - Added support to filter tasks that are ready to start, applying ordering and pagination.

---
+++ GraphT.Model/Services/Specifications/FindReadyToStartTasksSpecification.cs
@@ -0,0 +1,16 @@
+﻿using GraphT.Model.Aggregates;
+using GraphT.Model.ValueObjects;
+
+using SeedWork;
+
+namespace GraphT.Model.Services.Specifications;
+
+public sealed class FindReadyToStartTasksSpecification : BaseSpecification<TaskAggregate>
+{
+	public FindReadyToStartTasksSpecification(PagingParams pagingParams) : base(task => (task.Status == Status.ReadyToStart))
+	{
+		ApplyOrderByDescending(task => task.Priority);
+		AddThenBy(task => task.DateTimeInfo.LimitDateTime ?? DateTimeOffset.MaxValue);
+		ApplyPaging(pagingParams.PageNumber, pagingParams.PageSize);
+	}
+}
