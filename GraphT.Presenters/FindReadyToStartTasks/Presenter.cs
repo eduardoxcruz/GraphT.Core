1177131fda633ad5cd854cf575b9398109d84292
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-09 23:20:37 -0600

    Add FindReadyToStartTasks presenter

---
+++ GraphT.Presenters/FindReadyToStartTasks/Presenter.cs
@@ -0,0 +1,17 @@
+﻿using GraphT.UseCases.FindReadyToStartTasks;
+
+using SeedWork;
+
+namespace GraphT.Presenters.FindReadyToStartTasks;
+
+public class Presenter : IPresenter<OutputDto>, IOutputPort
+{
+	public OutputDto Content { get; private set; }
+
+	public ValueTask Handle(OutputDto dto)
+	{
+		Content = dto;
+		return ValueTask.CompletedTask;
+	}
+}
+
