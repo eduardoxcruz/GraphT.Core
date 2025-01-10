27468b05cd43f376645b192683c7745f07e472db
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-09 22:10:45 -0600

    Refactor FindUpstreamsByTaskIdSpecification to filter by Task ID and include upstreams instead of downstreams

--- GraphT.Model/Services/Specifications/FindUpstreamsByTaskIdSpecification.cs
+++ GraphT.Model/Services/Specifications/FindUpstreamsByTaskIdSpecification.cs
@@ -7,10 +7,10 @@ namespace GraphT.Model.Services.Specifications;
 public sealed class FindUpstreamsByTaskIdSpecification : BaseSpecification<TaskAggregate>
 {
 	public FindUpstreamsByTaskIdSpecification(Guid id, PagingParams pagingParams) : 
-		base(t => t.Downstreams.Contains(new TaskAggregate("", null, null, null, null, null, id)))
+		base(t => t.Id.Equals(id))
 	{
 		ApplyPaging(pagingParams.PageNumber, pagingParams.PageSize);
-		AddInclude(t => t.Downstreams);
+		AddInclude(t => t.Upstreams);
 	}
 }
 
