ecac45800f63466915c7a6c2136c4c0cbd02fd4b
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-09 21:28:26 -0600

    Add FindTaskUpstreamsById presenter

---
+++ GraphT.Presenters/FindTaskUpstreamsById/Presenter.cs
@@ -0,0 +1,17 @@
+﻿using GraphT.UseCases.FindTaskUpstreamsById;
+
+using SeedWork;
+
+namespace GraphT.Presenters.FindTaskUpstreamsById;
+
+public class Presenter : IPresenter<OutputDto>, IOutputPort
+{
+	public OutputDto Content { get; private set; }
+
+	public ValueTask Handle(OutputDto dto)
+	{
+		Content = dto;
+		return ValueTask.CompletedTask;
+	}
+}
+
