5f75baa74b84eb653fe69fa9352293f7315335e9
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-01-30 15:23:53 -0600

    Simplify UpdateRangeAsync method in Repository
    Replace manual entity state modification with UpdateRange method for more concise and efficient entity updates

--- GraphT.EfCore.Repositories/Repository.cs
+++ GraphT.EfCore.Repositories/Repository.cs
@@ -59,12 +59,7 @@ public class Repository<TEntity>(DbContext context) : IRepository<TEntity> where
 
 	public ValueTask UpdateRangeAsync(IEnumerable<TEntity> entities)
 	{
-		context.Set<TEntity>().AttachRange(entities);
-		
-		foreach (TEntity entity in entities)
-		{
-			context.Entry(entity).State = EntityState.Modified;
-		}
+		context.Set<TEntity>().UpdateRange(entities);
 		
 		return ValueTask.CompletedTask;
 	}
