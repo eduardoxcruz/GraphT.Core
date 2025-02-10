9e7de0a8735c43ce2e3f911ed8604475d35d4ae3
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-02-09 23:23:39 -0600

    Refactor Status enum to use EnumLabelAttribute

--- GraphT.Model/ValueObjects/Status.cs
+++ GraphT.Model/ValueObjects/Status.cs
@@ -1,31 +1,21 @@
-﻿namespace GraphT.Model.ValueObjects;
+﻿using GraphT.Model.ValueObjects.EnumLabel;
+
+namespace GraphT.Model.ValueObjects;
 
 public enum Status
 {
+	[EnumLabel("\u270c Created")]
 	Created = 0,
+	[EnumLabel("\ud83d\udcdd Backlog")]
 	Backlog = 1,
+	[EnumLabel("\ud83d\udc4c Ready To Start")]
 	Ready = 2,
+	[EnumLabel("\u25b6 Currently Doing")]
 	Doing = 3,
+	[EnumLabel("\u23f8 Paused")]
 	Paused = 4,
+	[EnumLabel("\ud83d\uddd1 Dropped")]
 	Dropped = 5,
+	[EnumLabel("\u2705 Completed")]
 	Completed = 6
-}
-
-public static class StatusExtensions
-{
-	public static string GetLabel(this Status status)
-	{
-		string formatedName = status switch
-		{
-			Status.Backlog => "\ud83d\udcdd Backlog",
-			Status.Ready => "\ud83d\udc4c Ready To Start",
-			Status.Doing => "\u25b6 Currently Doing",
-			Status.Paused => "\u23f8 Paused",
-			Status.Dropped => "\ud83d\uddd1 Dropped",
-			Status.Completed => "\u2705 Completed",
-			_ => "\u270c Created"
-		};
-
-		return formatedName;
-	}
 }
