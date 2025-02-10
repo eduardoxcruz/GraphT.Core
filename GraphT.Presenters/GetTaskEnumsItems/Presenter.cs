99fd89ce1d04da366dede3357f9ab0647ff0deee
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-02-09 23:26:51 -0600

    Add Presenter for GetTaskEnumsItems use case

---
+++ GraphT.Presenters/GetTaskEnumsItems/Presenter.cs
@@ -0,0 +1,17 @@
+﻿using GraphT.UseCases.GetTaskEnumsItems;
+
+using SeedWork;
+
+namespace GraphT.Presenters.GetTaskEnumsItems;
+
+public class Presenter : IPresenter<OutputDto>, IOutputPort
+{
+	public OutputDto Content { get; private set; }
+
+	public ValueTask Handle(OutputDto dto)
+	{
+		Content = dto;
+		return ValueTask.CompletedTask;
+	}
+}
+
