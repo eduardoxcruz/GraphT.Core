d7d6bf68836b2cf702cf19693fc06fdaeade92af
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-02-09 23:24:24 -0600

    Refactor GetTaskEnumsItems UseCase to use generic enum conversion method

--- GraphT.UseCases/GetTaskEnumsItems/UseCase.cs
+++ GraphT.UseCases/GetTaskEnumsItems/UseCase.cs
@@ -1,6 +1,5 @@
-using System.Numerics;
-
 using GraphT.Model.ValueObjects;
+using GraphT.Model.ValueObjects.EnumLabel;
 
 using SeedWork;
 
@@ -13,69 +12,31 @@ public interface IOutputPort : IPort<OutputDto> { }
 public class UseCase : IInputPort
 {
 	private readonly IOutputPort _outputPort;
-	private readonly IUnitOfWork _unitOfWork;
 
-	public UseCase(IOutputPort outputPort, IUnitOfWork unitOfWork)
+	public UseCase(IOutputPort outputPort)
 	{
 		_outputPort = outputPort;
-		_unitOfWork = unitOfWork;
 	}
 	
 	public async ValueTask Handle()
 	{
-		int item;
-		string label;
-		List<EnumItemAndLabel> complexityItems = new();
-		Array complexityEnumItems = Enum.GetValues(typeof(Complexity));
-		
-		foreach (Complexity complexity in complexityEnumItems)
+		var items = new List<List<EnumItemAndLabel>>
 		{
-			item = (int)complexity;
-			label = complexity.GetLabel();
-			EnumItemAndLabel enumItemAndLabel = new(item, label);
-			complexityItems.Add(enumItemAndLabel);
-		}
+			ConvertEnumToItems<Complexity>(),
+			ConvertEnumToItems<Priority>(),
+			ConvertEnumToItems<Relevance>(),
+			ConvertEnumToItems<Status>()
+		};
 
-		List<EnumItemAndLabel> priorityItems = new();
-		Array priorityEnumItems = Enum.GetValues(typeof(Priority));
-		
-		foreach (Priority priority in priorityEnumItems)
-		{
-			item = (int)priority;
-			label = priority.GetLabel();
-			EnumItemAndLabel enumItemAndLabel = new(item, label);
-			priorityItems.Add(enumItemAndLabel);
-		}
-		
-		List<EnumItemAndLabel> relevanceItems = new();
-		Array relevanceEnumItems = Enum.GetValues(typeof(Relevance));
-		
-		foreach (Relevance relevance in relevanceEnumItems)
-		{
-			item = (int)relevance;
-			label = relevance.GetLabel();
-			EnumItemAndLabel enumItemAndLabel = new(item, label);
-			relevanceItems.Add(enumItemAndLabel);
-		}
-		
-		List<EnumItemAndLabel> statusItems = new();
-		Array statusEnumItems = Enum.GetValues(typeof(Status));
-		
-		foreach (Status status in statusEnumItems)
-		{
-			item = (int)status;
-			label = status.GetLabel();
-			EnumItemAndLabel enumItemAndLabel = new(item, label);
-			statusItems.Add(enumItemAndLabel);
-		}
+		await _outputPort.Handle(new OutputDto { Items = items });
+	}
 
-		List<List<EnumItemAndLabel>> items = new();
-		items.Add(complexityItems);
-		items.Add(priorityItems);
-		items.Add(relevanceItems);
-		items.Add(statusItems);
-		
-		await _outputPort.Handle(new OutputDto(){ Items = items });
+	private static List<EnumItemAndLabel> ConvertEnumToItems<TEnum>() 
+		where TEnum : struct, Enum
+	{
+		return Enum.GetValues<TEnum>()
+			.Select(e => new EnumItemAndLabel(Convert.ToInt32(e), e.GetLabel()))
+			.ToList();
 	}
 }
 
