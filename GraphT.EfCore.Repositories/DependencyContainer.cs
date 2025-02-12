323ac5b515fbf370802960fa6daa105ac4b8588c
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-02-11 22:38:31 -0600

    Add debug logging to EF Core DbContext configuration

--- GraphT.EfCore.Repositories/DependencyContainer.cs
+++ GraphT.EfCore.Repositories/DependencyContainer.cs
@@ -1,9 +1,12 @@
+using System.Diagnostics;
+
 using GraphT.Model.Aggregates;
 using GraphT.Model.ValueObjects;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 using SeedWork;
 
@@ -16,7 +19,9 @@ public static class DependencyContainer
 		IConfiguration configuration,
 		string connectionString)
 	{
-		services.AddDbContext<EfDbContext>(options => options.UseSqlServer(configuration[$"ConnectionStrings:{connectionString}"]));
+		services.AddDbContext<EfDbContext>(options => options
+			.UseSqlServer(configuration[$"ConnectionStrings:{connectionString}"])
+			.LogTo(message => Debug.WriteLine(message), LogLevel.Warning));
 		services.AddScoped<IRepository<TodoTask>, Repository<TodoTask>>();
 		services.AddScoped<IRepository<TaskLog>, Repository<TaskLog>>();
 		services.AddScoped<IRepository<LifeArea>, Repository<LifeArea>>();
