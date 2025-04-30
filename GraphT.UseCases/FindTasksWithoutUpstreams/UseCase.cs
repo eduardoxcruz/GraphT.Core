2c6f0851120526aa68dcc316834d140c1837799f
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-04-29 23:30:59 -0600

    Add FindTasksWithoutUpstreams use case

---
+++ GraphT.UseCases/FindTasksWithoutUpstreams/UseCase.cs
@@ -0,0 +1,35 @@
+﻿using GraphT.Model.Aggregates;
+using GraphT.Model.Services.Specifications;
+
+using SeedWork;
+
+namespace GraphT.UseCases.FindTasksWithoutUpstreams;
+
+public interface IInputPort : IPort<InputDto> { }
+
+public interface IOutputPort : IPort<OutputDto> { }
+
+public class UseCase : IInputPort
+{
+	private readonly IOutputPort _outputPort;
+	private readonly IUnitOfWork _unitOfWork;
+
+	public UseCase(IOutputPort outputPort, IUnitOfWork unitOfWork)
+	{
+		_outputPort = outputPort;
+		_unitOfWork = unitOfWork;
+	}
+
+	public async ValueTask Handle(InputDto dto)
+	{
+		TasksWithoutUpstreamsSpecification specification = new(dto.PagingParams);
+		PagedList<TodoTask> tasks = await _unitOfWork.Repository<TodoTask>().FindAsync(specification);
+		
+		await _outputPort.Handle(new OutputDto(tasks));
+	}
+}
+
+public record struct InputDto(PagingParams PagingParams) {}
+
+public record struct OutputDto(PagedList<TodoTask> Tasks) {}
+
