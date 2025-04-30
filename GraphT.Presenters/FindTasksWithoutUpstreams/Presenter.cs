7d4165e370bdde5a85e9e1eb34fab41a3ab8d074
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-04-29 23:31:43 -0600

    Add FindTasksWithoutUpstreams presenter

---
+++ GraphT.Presenters/FindTasksWithoutUpstreams/Presenter.cs
@@ -0,0 +1,17 @@
+﻿using GraphT.UseCases.FindTasksWithoutUpstreams;
+
+using SeedWork;
+
+namespace GraphT.Presenters.FindTasksWithoutUpstreams;
+
+public class Presenter : IPresenter<OutputDto>, IOutputPort
+{
+	public OutputDto Content { get; private set; }
+
+	public ValueTask Handle(OutputDto dto)
+	{
+		Content = dto;
+		return ValueTask.CompletedTask;
+	}
+}
+
