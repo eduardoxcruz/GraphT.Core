7f868d38d7b65dcee5104b0b4c7c768795fd82c3
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-04-29 23:32:00 -0600

    Add FindTasksWithoutUpstreams controller

---
+++ GraphT.Controllers/FindTasksWithoutUpstreams/Controller.cs
@@ -0,0 +1,26 @@
+﻿using GraphT.UseCases.FindTasksWithoutUpstreams;
+
+using SeedWork;
+
+namespace GraphT.Controllers.FindTasksWithoutUpstreams;
+
+public interface IFindTasksWithoutUpstreamsController : IControllerTOutTIn<OutputDto, InputDto> {}
+
+public class Controller : IFindTasksWithoutUpstreamsController
+{
+	private readonly IInputPort _inputPort;
+	private readonly IOutputPort _outputPort;
+
+	public Controller(IInputPort inputPort, IOutputPort outputPort)
+	{
+		_inputPort = inputPort;
+		_outputPort = outputPort;
+	}
+    
+	public async ValueTask<OutputDto> RunUseCase(InputDto inputDto)
+	{
+		await _inputPort.Handle(inputDto);
+		return ((IPresenter<OutputDto>)_outputPort).Content;
+	}
+}
+
