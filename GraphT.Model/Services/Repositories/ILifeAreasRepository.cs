0af3d284d3a9ddec3e7ff2b6a8ed30c0b488f5a9
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-06-05 00:25:15 -0600

    Add AddAsync() and RemoveAsync() in ILifeAreasRepository.cs

--- GraphT.Model/Services/Repositories/ILifeAreasRepository.cs
+++ GraphT.Model/Services/Repositories/ILifeAreasRepository.cs
@@ -7,4 +7,6 @@ namespace GraphT.Model.Services.Repositories;
 public interface ILifeAreasRepository
 {
 	ValueTask<PagedList<LifeArea>> FindTaskLifeAreasById(Guid id);
+	ValueTask AddTaskAsync(Guid lifeAreaId, Guid taskId);
+	ValueTask RemoveTaskAsync(Guid lifeAreaId, Guid taskId);
 }
