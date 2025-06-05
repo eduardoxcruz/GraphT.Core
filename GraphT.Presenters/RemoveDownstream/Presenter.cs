85fd1d7e38ccc57068e3d2fa757d1c276fc502b1
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-06-05 00:28:19 -0600

    Add RemoveUpstream presenter

---
+++ GraphT.Presenters/RemoveDownstream/Presenter.cs
@@ -0,0 +1,12 @@
+﻿using GraphT.UseCases.RemoveDownstream;
+
+namespace GraphT.Presenters.RemoveDownstream;
+
+public class Presenter : IOutputPort
+{
+	public ValueTask Handle()
+	{
+		return ValueTask.CompletedTask;
+	}
+}
+
