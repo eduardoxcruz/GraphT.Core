dfa4757918fc23adf7cf55508d1dc3149b9beeba
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-06-05 00:44:16 -0600

    Add RemoveUpstream controller

---
+++ GraphT.Controllers/RemoveUpstream/Controller.cs
@@ -0,0 +1,25 @@
+﻿using GraphT.UseCases.RemoveUpstream;
+
+using SeedWork;
+
+namespace GraphT.Controllers.RemoveUpstream;
+
+public interface IRemoveUpstreamController : IControllerTIn<InputDto> {}
+
+public class Controller : IRemoveUpstreamController
+{
+	private readonly IInputPort _inputPort;
+	private readonly IOutputPort _outputPort;
+
+	public Controller(IInputPort inputPort, IOutputPort outputPort)
+	{
+		_inputPort = inputPort;
+		_outputPort = outputPort;
+	}
+    
+	public async ValueTask RunUseCase(InputDto inputDto)
+	{
+		await _inputPort.Handle(inputDto);
+	}
+}
+
