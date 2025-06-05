8e6f087b451af595624079b0f7849bd96b0ec1a9
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-06-05 00:43:58 -0600

    Add AddUpstream controller

---
+++ GraphT.Controllers/AddUpstream/Controller.cs
@@ -0,0 +1,25 @@
+﻿using GraphT.UseCases.AddUpstream;
+
+using SeedWork;
+
+namespace GraphT.Controllers.AddUpstream;
+
+public interface IAddUpstreamController : IControllerTIn<InputDto> {}
+
+public class Controller : IAddUpstreamController
+{
+	private readonly IInputPort _inputPort;
+	private readonly IOutputPort _outputPort;
+
+	public Controller(IInputPort inputPort, IOutputPort outputPort)
+	{
+		_inputPort = inputPort;
+		_outputPort = outputPort;
+	}
+    
+	public async ValueTask RunUseCase(InputDto inputDto)
+	{
+		await _inputPort.Handle(inputDto);
+	}
+}
+
