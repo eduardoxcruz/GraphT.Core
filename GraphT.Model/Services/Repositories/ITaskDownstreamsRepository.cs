fc4621b5e584c190f83d28e2d0f09c6d98589a19
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-06-05 00:24:56 -0600

    Add AddAsync() method in ITaskDownstreamsRepository.cs.cs

--- GraphT.Model/Services/Repositories/ITaskDownstreamsRepository.cs
+++ GraphT.Model/Services/Repositories/ITaskDownstreamsRepository.cs
@@ -7,4 +7,6 @@ namespace GraphT.Model.Services.Repositories;
 public interface ITaskDownstreamsRepository
 {
 	ValueTask<PagedList<TodoTask>> FindTaskDownstreamsById(Guid id);
+	ValueTask AddDownstreamAsync(Guid taskId, Guid downstreamId);
+	ValueTask RemoveDownstreamAsync(Guid taskId, Guid downstreamId);
 }
