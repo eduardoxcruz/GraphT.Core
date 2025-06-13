7111664fa88965dde87c6090805249d60e67cc8c
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-06-13 13:58:01 -0600

    Fix DateTimeInfo punctuality calculation

--- GraphT.Model/ValueObjects/DateTimeInfo.cs
+++ GraphT.Model/ValueObjects/DateTimeInfo.cs
@@ -23,24 +23,24 @@ public struct DateTimeInfo
 
 		if (FinishDateTime.HasValue)
 		{
-			if ((FinishDateTime.Value.Date - LimitDateTime.Value.Date).TotalDays == 0) return "\u2705 On Time!";
+			if ((FinishDateTime.Value - LimitDateTime.Value).TotalDays == 0) return "\u2705 On Time!";
 			
-			int daysDifference = Math.Abs((LimitDateTime.Value.Date - FinishDateTime.Value.Date).Days);
+			int daysDifference = Math.Abs((LimitDateTime.Value - FinishDateTime.Value).Days);
 			
-			return FinishDateTime.Value.Date > LimitDateTime.Value.Date ? 
+			return FinishDateTime.Value > LimitDateTime.Value ? 
 				$"\ud83d\udea8 Late {daysDifference} day(s)!" : 
 				$"\u2b50 Early {daysDifference} day(s)!";
 		}
 		
-		if ((LimitDateTime.Value.Date - now.Date).TotalDays == 0) return "\u26a0 Finish Today!";
+		if ((LimitDateTime.Value - now).TotalHours < 24) return "\u26a0 Finish Today!";
 		
 		if (now > LimitDateTime)
 		{
-			int daysLate = Math.Abs((LimitDateTime.Value.Date - now.Date).Days);
+			int daysLate = Math.Abs((LimitDateTime.Value - now).Days);
 			return $"\ud83d\udea8 Late {daysLate} day(s)!";
 		}
 		
-		int daysToGo = (LimitDateTime.Value.Date - now.Date).Days + 1;
+		int daysToGo = (LimitDateTime.Value - now).Days;
 		return $"\u23f1 {daysToGo} day(s) to go!";
 	}
 }
