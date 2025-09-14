ebbb967ed7213a98474818f826a2dba062d50b35
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-09-13 19:29:46 -0600

    Refactor `Relevance` to include `Index` property and predefined options; add corresponding unit tests

--- GraphT.Model/ValueObjects/Relevance.cs
+++ GraphT.Model/ValueObjects/Relevance.cs
@@ -2,26 +2,43 @@ namespace GraphT.Model.ValueObjects;
 
 public struct Relevance
 {
+	public int Index { get; }
 	public string Name { get; }
 	public string Emoji { get; }
 
+	public static Relevance Superfluous = new(0, "Superfluous", "\ud83d\ude12");
+	public static Relevance Entertaining = new(1, "Entertaining", "\ud83e\udd24");
+	public static Relevance Necessary = new(2, "Necessary", "\ud83e\uddd0");
+	public static Relevance Purposeful = new(3, "Purposeful", "\ud83d\ude0e");
+	
+	private Relevance(int index, string name, string emoji)
+	{
+		Index = index;
+		Name = name;
+		Emoji = emoji;
+	}
+	
 	public Relevance(bool isFun, bool isProductive)
 	{
 		switch (isFun, isProductive)
 		{
 			case (false, false):
+				Index = 0;
 				Name = "Superfluous";
 				Emoji = "\ud83d\ude12";
 				break;
 			case (true, false):
+				Index = 1;
 				Name = "Entertaining";
 				Emoji = "\ud83e\udd24";
 				break;
 			case (false, true):
+				Index = 2;
 				Name = "Necessary";
 				Emoji = "\ud83e\uddd0";
 				break;
 			case (true, true):
+				Index = 3;
 				Name = "Purposeful";
 				Emoji = "\ud83d\ude0e";
 				break;
