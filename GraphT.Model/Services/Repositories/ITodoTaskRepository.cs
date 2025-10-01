c931329cb3cfe53d79636d89e0ef14bb4b32f0cc
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-09-30 21:12:25 -0600

    Refactor `ITodoTaskRepository` interface: streamline methods for task retrieval by status, remove parent/child relations, and enhance clarity.

--- GraphT.Model/Services/Repositories/ITodoTaskRepository.cs
+++ GraphT.Model/Services/Repositories/ITodoTaskRepository.cs
@@ -1,4 +1,5 @@
 ﻿using GraphT.Model.Aggregates;
+using GraphT.Model.ValueObjects;
 
 using SeedWork;
 
@@ -6,20 +7,12 @@ namespace GraphT.Model.Services.Repositories;
 
 public interface ITodoTaskRepository
 {
-	ValueTask<TodoTask> FindByIdAsync(Guid id);
-	ValueTask<PagedList<TodoTask>> FindTasksCompletedOrDropped(PagingParams pagingParams);
-	ValueTask<PagedList<TodoTask>> FindTasksInProgress(PagingParams pagingParams);
-	ValueTask<PagedList<TodoTask>> FindTasksReadyToStart(PagingParams pagingParams);
+	ValueTask<PagedList<TodoTask>> FindByStatusAsync(PagingParams pagingParams, Status status);
+	ValueTask<PagedList<TodoTask>> FindTasksWithoutParentsAsync(PagingParams pagingParams);
 	ValueTask<PagedList<TodoTask>> GetTasksOrderedByCreationDateDescAsync(PagingParams pagingParams);
+	ValueTask<TodoTask> FindByIdAsync(Guid id);
 	ValueTask AddAsync(TodoTask task);
 	ValueTask UpdateAsync(TodoTask task);
 	ValueTask RemoveAsync(Guid id);
 	ValueTask<bool> ContainsAsync(Guid id);
-	ValueTask<PagedList<TodoTask>> FindParentsByIdAsync(Guid id);
-	ValueTask<PagedList<TodoTask>> FindTasksWithoutParentsAsync(PagingParams pagingParams);
-	ValueTask AddParentAsync(Guid taskId, Guid parentId);
-	ValueTask RemoveParentAsync(Guid taskId, Guid parentId);
-	ValueTask<PagedList<TodoTask>> FindChildrenById(Guid id);
-	ValueTask AddChildAsync(Guid taskId, Guid downstreamId);
-	ValueTask RemoveChildAsync(Guid taskId, Guid downstreamId);
 }
