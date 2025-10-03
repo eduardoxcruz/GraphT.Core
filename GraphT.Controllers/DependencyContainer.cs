fdb4b630ae9004421fd15944e60538597c3f768f
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-10-03 07:24:42 -0600

    Remove legacy controllers, presenters, and use cases to streamline `GraphT` architecture.

--- GraphT.Controllers/DependencyContainer.cs
+++ GraphT.Controllers/DependencyContainer.cs
@@ -1,22 +1,4 @@
-﻿using GraphT.Controllers.AddDownstream;
-using GraphT.Controllers.AddNewTask;
-using GraphT.Controllers.AddUpstream;
-using GraphT.Controllers.DeleteTask;
-using GraphT.Controllers.FindFinishedTasks;
-using GraphT.Controllers.FindInProgressTasks;
-using GraphT.Controllers.FindReadyToStartTasks;
-using GraphT.Controllers.FindTaskById;
-using GraphT.Controllers.FindTaskDownstreamsById;
-using GraphT.Controllers.FindTaskLifeAreasById;
-using GraphT.Controllers.FindTasksWithoutUpstreams;
-using GraphT.Controllers.UpdateTask;
-using GraphT.Controllers.FindTaskUpstreamsById;
-using GraphT.Controllers.GetTaskEnumsItems;
-using GraphT.Controllers.GetTasksOrderedByCreationDateDesc;
-using GraphT.Controllers.RemoveDownstream;
-using GraphT.Controllers.RemoveUpstream;
-
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace GraphT.Controllers;
 
@@ -24,24 +6,6 @@ public static class DependencyContainer
 {
 	public static IServiceCollection AddGraphTControllers(this IServiceCollection services)
 	{
-		services.AddScoped<IUpdateTaskController, UpdateTask.Controller>();
-		services.AddScoped<IAddNewTaskController, AddNewTask.Controller>();
-		services.AddScoped<IFindTaskByIdController, FindTaskById.Controller>();
-		services.AddScoped<IFindTaskUpstreamsByIdController, FindTaskUpstreamsById.Controller>();
-		services.AddScoped<IFindReadyToStartTasksController, FindReadyToStartTasks.Controller>();
-		services.AddScoped<IFindTaskDownstreamsByIdController, FindTaskDownstreamsById.Controller>();
-		services.AddScoped<IFindTaskLifeAreasByIdController, FindTaskLifeAreasById.Controller>();
-		services.AddScoped<IFindInProgressTasksController, FindInProgressTasks.Controller>();
-		services.AddScoped<IFindFinishedTasksController, FindFinishedTasks.Controller>();
-		services.AddScoped<IDeleteTaskController, DeleteTask.Controller>();
-		services.AddScoped<IGetTaskEnumsItemsController, GetTaskEnumsItems.Controller>();
-		services.AddScoped<IFindTasksWithoutUpstreamsController, FindTasksWithoutUpstreams.Controller>();
-		services.AddScoped<IAddDownstreamController, AddDownstream.Controller>();
-		services.AddScoped<IAddUpstreamController, AddUpstream.Controller>();
-		services.AddScoped<IRemoveDownstreamController, RemoveDownstream.Controller>();
-		services.AddScoped<IRemoveUpstreamController, RemoveUpstream.Controller>();
-		services.AddScoped<IGetTasksOrderedByCreationDateDescController, GetTasksOrderedByCreationDateDesc.Controller>();
-
 		return services;
 	}
 }
--- GraphT.UseCases/DependencyContainer.cs
+++ GraphT.UseCases/DependencyContainer.cs
@@ -6,24 +6,6 @@ public static class DependencyContainer
 {
 	public static IServiceCollection AddGraphTUseCases(this IServiceCollection services)
 	{
-		services.AddScoped<AddNewTask.IInputPort, AddNewTask.UseCase>();
-		services.AddScoped<UpdateTask.IInputPort, UpdateTask.UseCase>();
-		services.AddScoped<FindTaskUpstreamsById.IInputPort, FindTaskUpstreamsById.UseCase>();
-		services.AddScoped<FindTaskById.IInputPort, FindTaskById.UseCase>();
-		services.AddScoped<FindReadyToStartTasks.IInputPort, FindReadyToStartTasks.UseCase>();
-		services.AddScoped<FindTaskDownstreamsById.IInputPort, FindTaskDownstreamsById.UseCase>();
-		services.AddScoped<FindTaskLifeAreasById.IInputPort, FindTaskLifeAreasById.UseCase>();
-		services.AddScoped<FindInProgressTasks.IInputPort, FindInProgressTasks.UseCase>();
-		services.AddScoped<FindFinishedTasks.IInputPort, FindFinishedTasks.UseCase>();
-		services.AddScoped<DeleteTask.IInputPort, DeleteTask.UseCase>();
-		services.AddScoped<GetTaskEnumsItems.IInputPort, GetTaskEnumsItems.UseCase>();
-		services.AddScoped<FindTasksWithoutUpstreams.IInputPort, FindTasksWithoutUpstreams.UseCase>();
-		services.AddScoped<AddDownstream.IInputPort, AddDownstream.UseCase>();
-		services.AddScoped<AddUpstream.IInputPort, AddUpstream.UseCase>();
-		services.AddScoped<RemoveDownstream.IInputPort, RemoveDownstream.UseCase>();
-		services.AddScoped<RemoveUpstream.IInputPort, RemoveUpstream.UseCase>();
-		services.AddScoped<GetTasksOrderedByCreationDateDesc.IInputPort, GetTasksOrderedByCreationDateDesc.UseCase>();
-		
 		return services;
 	}
 }
