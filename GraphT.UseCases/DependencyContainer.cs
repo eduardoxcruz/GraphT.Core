3465f9c9241fbfa1d1c283f4b4d3b1a92af58a31
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2025-11-01 21:47:57 -0600

    Register `FindTaskById` use case in dependency container.

--- GraphT.UseCases/DependencyContainer.cs
+++ GraphT.UseCases/DependencyContainer.cs
@@ -10,6 +10,7 @@ public static class DependencyContainer
 	{
 		services.AddScoped<IFullPort<AddNewTask.InputDto, AddNewTask.OutputDto>, AddNewTask.UseCase>();
 		services.AddScoped<IPortWithInput<RemoveTask.InputDto>, RemoveTask.UseCase>();
+		services.AddScoped<IFullPort<FindTaskById.InputDto, FindTaskById.OutputDto>, FindTaskById.UseCase>();
 		
 		return services;
 	}
