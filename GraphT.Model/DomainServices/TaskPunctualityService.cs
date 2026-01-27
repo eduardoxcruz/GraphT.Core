e121dd961cdc607cfe3dbc1b31bc7f58cbe88fe3
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-01-27 00:42:55 -0600

    Add TaskPunctualityService.cs

---
+++ GraphT.Model/DomainServices/TaskPunctualityService.cs
@@ -0,0 +1,46 @@
+using GraphT.Model.Enums;
+using GraphT.Model.ValueObjects;
+
+namespace GraphT.Model.DomainServices;
+
+public static class TaskPunctualityService
+{
+	public static Punctuality Calculate(DateTimeOffset? limitDate, DateTimeOffset? finishDate)
+	{
+		const int totalMillisecondsInADay = 86_400_000;
+		
+		if (limitDate is null) return new Punctuality(TimeSpan.Zero, PunctualityType.NoLimit);
+
+		TimeSpan timeDifference;
+		
+		if (finishDate is not null)
+		{
+			timeDifference = finishDate.Value - limitDate.Value;
+
+			switch (timeDifference.TotalMilliseconds)
+			{
+				case 0: return new Punctuality(timeDifference, PunctualityType.OnTime);
+				case < 0: return timeDifference.TotalMilliseconds < -totalMillisecondsInADay ? 
+					new Punctuality(timeDifference, PunctualityType.Early): 
+					new Punctuality(timeDifference, PunctualityType.OnTime);
+				case > 0: return timeDifference.TotalSeconds < 1 ? 
+					new Punctuality(timeDifference, PunctualityType.OnTime) : 
+					new Punctuality(timeDifference, PunctualityType.Late);
+			}
+		}
+
+		timeDifference = (limitDate.Value - DateTimeOffset.Now);
+		const int marginOfError = 10;
+		
+		if (timeDifference.Seconds > 0) timeDifference = timeDifference.Add(TimeSpan.FromMilliseconds(marginOfError));
+		
+		if (timeDifference.TotalMilliseconds < (totalMillisecondsInADay + marginOfError))
+		{
+			return timeDifference.TotalMilliseconds >= -marginOfError ? 
+				new Punctuality(timeDifference, PunctualityType.FinishToday) : 
+				new Punctuality(timeDifference, PunctualityType.Late);
+		}
+		
+		return new Punctuality(timeDifference, PunctualityType.TimeRemaining);
+	}
+}
