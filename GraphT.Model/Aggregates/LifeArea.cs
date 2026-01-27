17a09e64f7a8918bfd40177b358ae7fd916d71a7
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-01-27 00:42:34 -0600

    Add LifeArea as entity

---
+++ GraphT.Model/Aggregates/LifeArea.cs
@@ -0,0 +1,69 @@
+using SeedWork;
+
+namespace GraphT.Model.Aggregates;
+
+public class LifeArea : IEntity<Guid>, IEquatable<LifeArea>
+{
+	public Guid Id { get; }
+	public string Name { get; private set; } = string.Empty;
+
+    private LifeArea() { }
+	
+    private LifeArea(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Life area name cannot be empty.", nameof(name));
+
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+
+    public bool Equals(LifeArea? other)
+    {
+	    if (other is null)
+	    {
+		    return false;
+	    }
+
+	    if (ReferenceEquals(this, other))
+	    {
+		    return true;
+	    }
+
+	    return Id.Equals(other.Id);
+    }
+
+    public override bool Equals(object? obj)
+    {
+	    if (obj is null)
+	    {
+		    return false;
+	    }
+
+	    if (ReferenceEquals(this, obj))
+	    {
+		    return true;
+	    }
+
+	    if (obj.GetType() != GetType())
+	    {
+		    return false;
+	    }
+
+	    return Equals((LifeArea)obj);
+    }
+
+    public override int GetHashCode()
+    {
+	    return Id.GetHashCode();
+    }
+
+    public static bool operator ==(LifeArea? left, LifeArea? right)
+    {
+	    return Equals(left, right);
+    }
+
+    public static bool operator !=(LifeArea? left, LifeArea? right)
+    {
+	    return !Equals(left, right);
+    }
+}
