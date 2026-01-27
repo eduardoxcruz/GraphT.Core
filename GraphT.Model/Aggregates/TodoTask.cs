2e847265b27a5c2ac2c6cd7cc1c63ab09a7e20ac
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-01-27 01:40:00 -0600

    Refactor `TodoTask` to improve encapsulation, add task management methods, introduce `TaskState` logic, and integrate domain services for calculated properties.

--- GraphT.Model/Aggregates/TodoTask.cs
+++ GraphT.Model/Aggregates/TodoTask.cs
@@ -1,78 +1,81 @@
+using GraphT.Model.DomainServices;
+using GraphT.Model.Enums;
 using GraphT.Model.ValueObjects;
+
+using SeedWork;
 
 namespace GraphT.Model.Aggregates;
 
-public class TodoTask : IEquatable<TodoTask>
+public class TodoTask : IEntity<Guid>, IEquatable<TodoTask>
 {
-	private List<TodoTask> _parents;
-	private List<TodoTask> _children;
-	private List<LifeArea> _lifeAreas;
-	private LinkedList<StatusChangelog> _statusChangeLogs;
-	
 	public Guid Id { get; }
-	public string Name { get; set; }
-	public bool IsFun { get; set; }
-	public bool IsProductive { get; set; }
-	public Complexity Complexity { get; set; }
-	public Priority Priority { get; set; }
+	public string Name { get; private set; }
+	public bool IsFun { get; private set; }
+	public bool IsProductive { get; private set; }
+	public Complexity Complexity { get; private set; }
+	public Priority Priority { get; private set; }
+	public TaskState Status { get; private set; }
 	
-	public Status Status { get; private set; }
+	public DateTimeOffset CreatedAt { get; }
+	public DateTimeOffset? StartDate { get; private set; }
+	public DateTimeOffset? FinishDate { get; private set; }
 	public DateTimeOffset? LimitDateTime { get; private set; }
-	
-	public Relevance Relevance => new(IsFun, IsProductive);
-	public string Punctuality => GetPunctuality();
-	public double Progress => GetProgress();
-	public TimeSpan ElapsedTime => GetElapsedTime();
-	public string ElapsedTimeFormatted => ElapsedTime.ToElapsedTime();
+	public DateTimeOffset? CurrentWorkSessionStartedAt { get; private set; }
+	public TimeSpan ElapsedTime { get; private set; }
 
-	public IReadOnlyCollection<StatusChangelog> StatusChangeLogs => _statusChangeLogs;
+	public Relevance Relevance => TaskRelevanceService.Calculate(IsFun, IsProductive);
+	public int Progress => TaskProgressService.Calculate(_children, Status);
+	public Punctuality Punctuality => TaskPunctualityService.Calculate(LimitDateTime, FinishDate);
+	
+	private List<TodoTask> _parents;
 	public IReadOnlyList<TodoTask> Parents => _parents;
+	
+	private List<TodoTask> _children;
 	public IReadOnlyList<TodoTask> Children => _children;
+	
+	private List<LifeArea> _lifeAreas;
 	public IReadOnlyList<LifeArea> LifeAreas => _lifeAreas;
 	
-	public TodoTask() : this("New Todo Task") {}
-	
-	public TodoTask(string name)
-	{
-		if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name cannot be empty");
-		
-		Id = Guid.NewGuid();
-		Name = name;
-		Complexity = Complexity.Indefinite;
-		Priority = Priority.Distraction;
-		Status = Status.Created;
-		_statusChangeLogs = [];
-		_statusChangeLogs.AddFirst(new StatusChangelog(DateTimeOffset.Now, Status.Created));
-		_parents = [];
-		_children = [];
-		_lifeAreas = [];
-	}
+	private List<StatusChangelog> _statusChangeLogs;
+	public IReadOnlyList<StatusChangelog> StatusChangeLogs => _statusChangeLogs;
 
+	public TodoTask() : this("New Task") {}
+	
 	public TodoTask(string name, 
-		Status? status = null,
+		bool? isFun = null, 
+		bool? isProductive = null, 
+		Complexity? complexity = null, 
+		Priority? priority = null, 
+		TaskState? status = null, 
+		DateTimeOffset? startDate = null, 
+		DateTimeOffset? finishDate = null, 
 		DateTimeOffset? limitDateTime = null,
-		LinkedList<StatusChangelog>? statusChangeLogs = null, 
 		List<TodoTask>? parents = null, 
 		List<TodoTask>? children = null, 
-		List<LifeArea>? lifeAreas = null)
+		List<LifeArea>? lifeAreas = null, 
+		List<StatusChangelog>? statusChangeLogs = null)
 	{
 		if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name cannot be empty");
 		
 		Id = Guid.NewGuid();
+		CreatedAt = DateTimeOffset.Now;
 		Name = name;
-		Complexity = Complexity.Indefinite;
-		Priority = Priority.Distraction;
-		Status = status ?? Status.Created;
+		IsFun = isFun ?? false;
+		IsProductive = isProductive ?? false;
+		Complexity = complexity ?? Complexity.Undefined;
+		Priority = priority ?? Priority.Distraction;
+		Status = status ?? TaskState.Created;
+		StartDate = startDate;
+		FinishDate = finishDate;
 		LimitDateTime = limitDateTime;
 		_parents = parents ?? [];
 		_children = children ?? [];
 		_lifeAreas = lifeAreas ?? [];
-
+		
 		switch (statusChangeLogs is null)
 		{
 			case true:
-				_statusChangeLogs = [];
-				_statusChangeLogs.AddFirst(new StatusChangelog(DateTimeOffset.Now, Status.Created));
+				_statusChangeLogs = [ new StatusChangelog(DateTimeOffset.Now, TaskState.Created) ];
 				break;
 			case false:
 				_statusChangeLogs = statusChangeLogs;
@@ -80,65 +83,59 @@ public class TodoTask : IEquatable<TodoTask>
 		}
 	}
 	
-	public void SetStatus(Status newStatus)
+	public void SetValues(string? name = null, 
+		bool? isFun = null, 
+		bool? isProductive = null, 
+		Complexity? complexity = null, 
+		Priority? priority = null, 
+		DateTimeOffset? startDate = null, 
+		DateTimeOffset? finishDate = null, 
+		DateTimeOffset? limitDateTime = null)
 	{
-		SetStatus(DateTimeOffset.Now, newStatus);
+		if (name is not null) Name = name;
+		
+		if (isFun is not null) IsFun = isFun.Value;
+		
+		if (isProductive is not null) IsProductive = isProductive.Value;
+		
+		if (complexity is not null) Complexity = complexity.Value;
+		
+		if (priority is not null) Priority = priority.Value;
+		
+		if (startDate is not null) StartDate = startDate.Value;
+		
+		if (finishDate is not null) FinishDate = finishDate.Value;
+		
+		if (limitDateTime is not null) LimitDateTime = limitDateTime.Value;
 	}
 	
-	public void SetStatus(DateTimeOffset dateTime, Status newStatus)
+	public DomainResult SetStatus(TaskState newStatus, DateTimeOffset? dateTime = null)
 	{
-		if (newStatus == this.Status) return;
+		if (newStatus == Status) return DomainResult.Failure("Task already have this state.");
 		
-		_statusChangeLogs.AddLast(new StatusChangelog(dateTime, newStatus));
+		dateTime ??= DateTimeOffset.Now;
+
+		if (Status == TaskState.Doing && newStatus != TaskState.Doing)
+		{
+			ElapsedTime += (dateTime.Value) - CurrentWorkSessionStartedAt!.Value;
+			CurrentWorkSessionStartedAt = null;
+		}
 		
+		switch (newStatus)
+		{
+			case TaskState.Doing:
+				CurrentWorkSessionStartedAt = dateTime;
+				break;
+			case TaskState.Discarded or TaskState.Finished:
+				FinishDate = dateTime;
+				break;
+		}
+
 		Status = newStatus;
-	}
-	
-	public void SetLimitDateTime(DateTimeOffset dateTime)
-	{
-		LimitDateTime = dateTime;
-	}
-	
-	private string GetPunctuality()
-	{
-		if (LimitDateTime is null) return "\u26a0 No Target";
-
-		StatusChangelog lastLog = _statusChangeLogs.Last!.Value;
-
-		bool lastLogIsCompletedOrDropped = Equals(lastLog.Status, Status.Completed) || Equals(lastLog.Status, Status.Dropped);
-
-		TimeSpan timeDifference;
 		
-		if (lastLogIsCompletedOrDropped)
-		{
-			timeDifference = lastLog.ChangeDateTime - LimitDateTime.Value;
-
-			switch (timeDifference.TotalMilliseconds)
-			{
-				case 0: return "\u2705 On Time!";
-				case > 0:
-					{
-						if (timeDifference.TotalSeconds < 1) return "\u2705 On Time!";
-						
-						return $"\ud83d\udea8 Late {Math.Abs(timeDifference.Days)} day(s) - {Math.Abs(timeDifference.Hours)} hours(s) - {Math.Abs(timeDifference.Minutes)} minute(s) - {Math.Abs(timeDifference.Seconds)} second(s)!";
-					}
-				case < 0: return timeDifference.TotalMilliseconds < -86_400_000 ? $"\u2b50 Early {Math.Abs(timeDifference.Days)} day(s) - {Math.Abs(timeDifference.Hours)} hours(s) - {Math.Abs(timeDifference.Minutes)} minute(s) - {Math.Abs(timeDifference.Seconds)} second(s)!" : "\u2705 On Time!";
-			}
-		}
+		_statusChangeLogs.Add(new StatusChangelog(dateTime.Value, newStatus));
 		
-		DateTimeOffset now = DateTimeOffset.Now;
-		timeDifference = (LimitDateTime.Value - now);
-		
-		if (timeDifference.Seconds > 0) timeDifference = timeDifference.Add(TimeSpan.FromMilliseconds(10));
-		
-		if (timeDifference.TotalMilliseconds < 86_400_010)
-		{
-			if (timeDifference.TotalMilliseconds >= -10) return "\u26a0 Finish Today!";
-			
-			return $"\ud83d\udea8 Late {Math.Abs(timeDifference.Days)} day(s) - {Math.Abs(timeDifference.Hours)} hours(s) - {Math.Abs(timeDifference.Minutes)} minute(s) - {Math.Abs(timeDifference.Seconds)} second(s)!";
-		}
-		
-		return $"\u23f1 {Math.Abs(timeDifference.Days)} day(s) - {Math.Abs(timeDifference.Hours)} hours(s) - {Math.Abs(timeDifference.Minutes)} minute(s) - {Math.Abs(timeDifference.Add(TimeSpan.FromMilliseconds(10)).Seconds)} second(s) To Go!";
+		return DomainResult.Success();
 	}
 	
 	public void AddParents(List<TodoTask> parents)
@@ -161,48 +158,43 @@ public class TodoTask : IEquatable<TodoTask>
 		}
 	}
 
-	private TimeSpan GetElapsedTime()
-	{
-		TimeSpan elapsedTime = TimeSpan.Zero;
-		LinkedListNode<StatusChangelog> log = _statusChangeLogs.First!;
-
-		while (log is not null)
-		{
-			if (log.Previous is null || !Equals(log.Previous.Value.Status, Status.CurrentlyDoing))
-			{
-				log = log.Next;
-				continue;
-			}
-
-			TimeSpan temporal = log.Value.ChangeDateTime - log.Previous.Value.ChangeDateTime;
-			elapsedTime += temporal;
-			log = log.Next;
-		}
-		
-		return elapsedTime;
-	}
-
-	private double GetProgress()
-	{
-		int totalChildren = _children.Count;
-		
-		if (Status.Index > 4) return 100;
-
-		if (totalChildren == 0) return 0;
-		
-		int completedChildren = _children.Count(t => t.Status.Index > 4);
-		
-		if (completedChildren == _children.Count) return 99;
-		
-		return (completedChildren * 100) / totalChildren;
-	}
-
 	public void AddLifeAreas(List<LifeArea> lifeAreas)
 	{
 		foreach (LifeArea lifeArea in lifeAreas)
 		{
 			if (!_lifeAreas.Contains(lifeArea)) _lifeAreas.Add(lifeArea);
 		}
+	}
+	
+	public void RemoveParents(List<TodoTask> parents)
+	{
+		_parents.RemoveAll(parents.Contains);
+	}
+
+	public void RemoveChildren(List<TodoTask> children)
+	{
+		_children.RemoveAll(children.Contains);
+	}
+
+	public void RemoveLifeAreas(List<LifeArea> lifeAreas)
+	{
+		_lifeAreas.RemoveAll(lifeAreas.Contains);
+	}
+	
+	public TodoTask Split()
+	{
+		TodoTask newTask = new(
+			$"{Name} (2)",
+			IsFun,
+			IsProductive,
+			Complexity,
+			Priority,
+			lifeAreas: _lifeAreas);
+
+		AddChildren([ newTask ]);
+		newTask.AddParents([ this ]);
+		
+		return newTask;
 	}
 
 	public bool Equals(TodoTask? other)
@@ -253,20 +245,5 @@ public class TodoTask : IEquatable<TodoTask>
 	public static bool operator !=(TodoTask? left, TodoTask? right)
 	{
 		return !Equals(left, right);
-	}
-
-	public void RemoveParents(List<TodoTask> parents)
-	{
-		_parents.RemoveAll(parents.Contains);
-	}
-
-	public void RemoveChildren(List<TodoTask> children)
-	{
-		_children.RemoveAll(children.Contains);
-	}
-
-	public void RemoveLifeAreas(List<LifeArea> lifeAreas)
-	{
-		_lifeAreas.RemoveAll(lifeAreas.Contains);
 	}
 }
