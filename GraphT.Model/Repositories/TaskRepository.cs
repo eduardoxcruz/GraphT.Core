12a6d3110d6b4f35d336990cfb762527249514f6
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-04 01:13:40 -0600

    Add new task management interfaces and DTOs to `TaskRepository` for enhanced task relationship handling

--- GraphT.Model/Repositories/TaskRepository.cs
+++ GraphT.Model/Repositories/TaskRepository.cs
@@ -8,6 +8,13 @@ namespace GraphT.Model.Repositories;
 public interface IGetParentsByIdPort : IFullPort<Guid, List<TodoTask>>;
 public interface IGetChildrenStatusesByIdPort : IFullPort<Guid, List<TaskState>>;
 public interface IUpdateTaskStatusPort : IPortWithInput<UpdateTaskStatusDto>;
+public interface ITaskHasParentsAndChildrenPort : IFullPort<Guid, bool>;
+public interface IGetChildrenByIdPort : IFullPort<Guid, List<TodoTask>>;
+public interface IDeleteTaskByIdPort : IPortWithInput<Guid>;
+public interface ITaskHasOnlyThisParentPort : IFullPort<TaskHasOnlyThisParentDto, bool>;
+public interface IAddParentByIdPort : IPortWithInput<AddParentDto>;
 public interface IUpdateTaskPriorityPort : IPortWithInput<Priority>;
 
+public record struct AddParentDto(Guid TaskId, Guid ParentId);
+public record struct TaskHasOnlyThisParentDto(Guid TaskId, Guid ParentId);
 public record struct UpdateTaskStatusDto(Guid TaskId, TaskState TaskStatus);
