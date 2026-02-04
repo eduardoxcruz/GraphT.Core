347474e86139f4bc3f0db99075865a4f9ad2c19e
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-04 17:08:14 -0600

    Add new interfaces and DTOs to `TaskRepository` for managing parent relationships and task existence checks

--- GraphT.Model/Repositories/TaskRepository.cs
+++ GraphT.Model/Repositories/TaskRepository.cs
@@ -9,10 +9,12 @@ public interface IGetParentsCountPort : IFullPort<Guid, int>;
 public interface ITaskHasOnlyThisParentPort : IFullPort<TaskHasOnlyThisParentDto, bool>;
 public interface IAddParentByIdPort : IPortWithInput<AddParentDto>;
 public interface IGetParentsByIdPort : IFullPort<Guid, List<TodoTask>>;
+public interface IRemoveParentById : IPortWithInput<RemoveParentDto>;
 
-public interface IGetChildrenByIdPort : IFullPort<Guid, List<TodoTask>>;
+public interface IGetChildrenByIdPortPort : IFullPort<Guid, List<TodoTask>>;
 public interface IGetOnlyChildrenIdsPort : IFullPort<Guid, List<Guid>>;
 
+public interface ITaskExistPort : IFullPort<Guid, bool>;
 public interface IDeleteTaskByIdPort : IPortWithInput<Guid>;
 public interface IGetTaskByIdPort : IFullPort<Guid, TodoTask?>;
 
@@ -21,5 +23,6 @@ public interface IUpdateTaskStatusPort : IPortWithInput<UpdateTaskStatusDto>;
 
 public record struct IUpdateTaskPriorityDto(Guid TaskId, Priority PriorityValue);
 public record struct AddParentDto(Guid TaskId, Guid ParentId);
+public record struct RemoveParentDto(Guid TaskId, Guid ParentId);
 public record struct TaskHasOnlyThisParentDto(Guid TaskId, Guid ParentId);
 public record struct UpdateTaskStatusDto(Guid TaskId, TaskState TaskStatus);
