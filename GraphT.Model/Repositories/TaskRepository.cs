96c47aa307e187669774b3267c60df3fd92256a8
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-04 17:00:54 -0600

    Reorder interfaces in TaskRepository.cs

--- GraphT.Model/Repositories/TaskRepository.cs
+++ GraphT.Model/Repositories/TaskRepository.cs
@@ -5,16 +5,19 @@ using SeedWork;
 
 namespace GraphT.Model.Repositories;
 
-public interface IGetParentsByIdPort : IFullPort<Guid, List<TodoTask>>;
-public interface IUpdateTaskStatusPort : IPortWithInput<UpdateTaskStatusDto>;
-public interface IGetOnlyChildrenIdsPort : IFullPort<Guid, List<Guid>>;
 public interface IGetParentsCountPort : IFullPort<Guid, int>;
-public interface IDeleteTaskByIdPort : IPortWithInput<Guid>;
 public interface ITaskHasOnlyThisParentPort : IFullPort<TaskHasOnlyThisParentDto, bool>;
 public interface IAddParentByIdPort : IPortWithInput<AddParentDto>;
-public interface IGetTaskByIdPort : IFullPort<Guid, TodoTask?>;
+public interface IGetParentsByIdPort : IFullPort<Guid, List<TodoTask>>;
+
 public interface IGetChildrenByIdPort : IFullPort<Guid, List<TodoTask>>;
+public interface IGetOnlyChildrenIdsPort : IFullPort<Guid, List<Guid>>;
+
+public interface IDeleteTaskByIdPort : IPortWithInput<Guid>;
+public interface IGetTaskByIdPort : IFullPort<Guid, TodoTask?>;
+
 public interface IUpdateTaskPriorityPort : IPortWithInput<IUpdateTaskPriorityDto>;
+public interface IUpdateTaskStatusPort : IPortWithInput<UpdateTaskStatusDto>;
 
 public record struct IUpdateTaskPriorityDto(Guid TaskId, Priority PriorityValue);
 public record struct AddParentDto(Guid TaskId, Guid ParentId);
