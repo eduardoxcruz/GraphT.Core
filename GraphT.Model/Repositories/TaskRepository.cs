43e0758fcc4e224554b5150fc20627d971fae0c8
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-04 18:25:42 -0600

    Rename `IUpdateTaskPriorityDto` to `UpdateTaskPriorityDto` for consistency with naming conventions

--- GraphT.Model/Repositories/TaskRepository.cs
+++ GraphT.Model/Repositories/TaskRepository.cs
@@ -10,8 +10,8 @@ public interface ITaskExistPort : IFullPort<Guid, bool>;
 public interface IDeleteTaskByIdPort : IPortWithInput<Guid>;
 public interface IGetTaskByIdPort : IFullPort<Guid, TodoTask?>;
 
-public interface IUpdateTaskPriorityPort : IPortWithInput<IUpdateTaskPriorityDto>;
+public interface IUpdateTaskPriorityPort : IPortWithInput<UpdateTaskPriorityDto>;
 public interface IUpdateTaskStatusPort : IPortWithInput<UpdateTaskStatusDto>;
 
-public record struct IUpdateTaskPriorityDto(Guid TaskId, Priority PriorityValue);
+public record struct UpdateTaskPriorityDto(Guid TaskId, Priority PriorityValue);
 public record struct UpdateTaskStatusDto(Guid TaskId, TaskState TaskStatus);
