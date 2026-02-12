48a62cccba93abb710dc685f237437aa0bde52a9
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-11 22:42:53 -0600

    Fix name of port

--- GraphT.Model/Repositories/TaskChildRepository.cs
+++ GraphT.Model/Repositories/TaskChildRepository.cs
@@ -4,5 +4,5 @@ using SeedWork;
 
 namespace GraphT.Model.Repositories;
 
-public interface IGetChildrenByIdPortPort : IFullPort<Guid, List<TodoTask>>;
+public interface IGetChildrenByIdPort : IFullPort<Guid, List<TodoTask>>;
 public interface IGetOnlyChildrenIdsPort : IFullPort<Guid, List<Guid>>;
