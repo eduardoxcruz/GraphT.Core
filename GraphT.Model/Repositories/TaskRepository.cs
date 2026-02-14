7f5b4dfc2f14f9d907b4afe01930003d14923c13
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-14 15:18:34 -0600

    Rename interface

--- GraphT.Model/Repositories/TaskRepository.cs
+++ GraphT.Model/Repositories/TaskRepository.cs
@@ -9,13 +9,15 @@ public interface IAddTaskPort : IPortWithInput<TodoTask>;
 public interface ITaskExistPort : IFullPort<Guid, bool>;
 public interface IDeleteTaskByIdPort : IPortWithInput<Guid>;
 public interface IFindTaskByIdPort : IFullPort<Guid, TodoTask?>;
+public interface IFindRecurringTasksPort : IPortWithOutput<List<TodoTask>>;
 
 public interface IUpdateTaskPriorityPort : IPortWithInput<UpdateTaskPriorityDto>;
-public interface IUpdateTaskStatusPort : IPortWithInput<UpdateTaskStatusDto>;
-public interface IUpdateTaskBasicInfo : IPortWithInput<TodoTask>;
-public interface IUpdateTaskDateTimes : IPortWithInput<TodoTask>;
 
-public interface IFindRecurringTasks : IPortWithOutput<List<TodoTask>>;
+public interface IUpdateTaskStatusPort : IPortWithInput<UpdateTaskStatusDto>;
+
+public interface IUpdateTaskBasicInfo : IPortWithInput<TodoTask>;
+
+public interface IUpdateTaskDateTimes : IPortWithInput<TodoTask>;
 
 public record struct UpdateTaskPriorityDto(Guid TaskId, Priority PriorityValue);
 public record struct UpdateTaskStatusDto(Guid TaskId, TaskState TaskStatus);
