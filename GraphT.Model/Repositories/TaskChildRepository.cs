f599a20a3c4d9e509589fb79230899ce80126ae2
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-14 14:16:05 -0600

    Rename to IFindChildrenByParentIdPort

--- GraphT.Model/Repositories/TaskChildRepository.cs
+++ GraphT.Model/Repositories/TaskChildRepository.cs
@@ -4,5 +4,5 @@ using SeedWork;
 
 namespace GraphT.Model.Repositories;
 
-public interface IGetChildrenByIdPort : IFullPort<Guid, List<TodoTask>>;
+public interface IFindTaskChildrenPort : IFullPort<Guid, List<TodoTask>>;
 public interface IGetOnlyChildrenIdsPort : IFullPort<Guid, List<Guid>>;
