cb103a2159bab65c6bfc37622096aa66794e4be9
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-14 15:05:07 -0600

    Rename interface

--- GraphT.Model/Repositories/TaskParentRepository.cs
+++ GraphT.Model/Repositories/TaskParentRepository.cs
@@ -6,10 +6,10 @@ namespace GraphT.Model.Repositories;
 
 public interface IGetParentsCountByChildIdPort : IFullPort<Guid, int>;
 public interface IChildHasSingleParentWithIdPort : IFullPort<TaskHasOnlyThisParentDto, bool>;
-public interface IAddParentByIdPort : IPortWithInput<AddParentDto>;
+public interface ILinkParentIdToChildId : IPortWithInput<LinkParentIdToChildIdDto>;
 public interface IGetParentsByIdPort : IFullPort<Guid, List<TodoTask>>;
 public interface IRemoveParentById : IPortWithInput<RemoveParentDto>;
 
-public record struct AddParentDto(Guid TaskId, Guid ParentId);
+public record struct LinkParentIdToChildIdDto(Guid ChildId, Guid ParentId);
 public record struct RemoveParentDto(Guid TaskId, Guid ParentId);
 public record struct TaskHasOnlyThisParentDto(Guid TaskId, Guid ParentId);
