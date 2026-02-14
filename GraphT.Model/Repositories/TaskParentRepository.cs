46b59261d741f999814043fbca5a0514b4b8837d
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-14 15:09:26 -0600

    Rename dto

--- GraphT.Model/Repositories/TaskParentRepository.cs
+++ GraphT.Model/Repositories/TaskParentRepository.cs
@@ -5,11 +5,11 @@ using SeedWork;
 namespace GraphT.Model.Repositories;
 
 public interface IGetParentsCountByChildIdPort : IFullPort<Guid, int>;
-public interface IChildHasSingleParentWithIdPort : IFullPort<TaskHasOnlyThisParentDto, bool>;
+public interface IChildHasSingleParentWithIdPort : IFullPort<ChildHasSingleParentWithIdDto, bool>;
 public interface ILinkParentIdToChildIdPort : IPortWithInput<LinkParentIdToChildIdDto>;
 public interface IFindParentsByChildIdPort : IFullPort<Guid, List<TodoTask>>;
 public interface IRemoveParentById : IPortWithInput<RemoveParentDto>;
 
 public record struct LinkParentIdToChildIdDto(Guid ChildId, Guid ParentId);
 public record struct RemoveParentDto(Guid TaskId, Guid ParentId);
-public record struct TaskHasOnlyThisParentDto(Guid TaskId, Guid ParentId);
+public record struct ChildHasSingleParentWithIdDto(Guid TaskId, Guid ParentId);
