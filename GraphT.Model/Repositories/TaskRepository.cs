5e74f4d82c9f1ef6fa87f1c4987b848a922609f7
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2026-02-14 22:23:38 -0600

    Remove unused interfaces, add IFindTasksByIdPort

--- GraphT.Model/Repositories/TaskRepository.cs
+++ GraphT.Model/Repositories/TaskRepository.cs
@@ -9,10 +9,7 @@ public interface IAddTaskPort : IPortWithInput<TodoTask>;
 public interface IUpdateTaskPort : IPortWithInput<TodoTask>;
 public interface IDeleteTaskByIdPort : IPortWithInput<Guid>;
 public interface ITaskExistPort : IFullPort<Guid, bool>;
-public interface IFindTaskByIdPort : IFullPort<FindTaskByIdIncludesDto, TodoTask?>;
+
+public interface IFindTaskByIdPort : IFullPort<Guid, TodoTask?>;
 public interface IFindRecurringTasksPort : IPortWithOutput<List<TodoTask>>;
-
-public interface IFindChildrenByParentIdPort : IFullPort<Guid, List<TodoTask>>;
-public interface IFindParentsByChildIdPort : IFullPort<Guid, List<TodoTask>>;
-
-public record struct FindTaskByIdIncludesDto(Guid TaskId, bool IncludeStatusLogs = false);
+public interface IFindTasksByIdPort : IFullPort<List<Guid>, List<TodoTask>>;
